17e9b33f9726a45bbe047c31150c2950d1e939ff
korablique <yulia-11a@yandex.ru>
2018-07-14 12:48:31 +0700

    show error when text is empty

--- app/src/main/java/korablique/softomatetestapp/NewTextActivity.java
+++ app/src/main/java/korablique/softomatetestapp/NewTextActivity.java
@@ -27,7 +27,11 @@ public class NewTextActivity extends BaseActivity {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(v -> {
             EditText newText = findViewById(R.id.new_text);
-            String text = newText.getText().toString();
+            String text = newText.getText().toString().trim();
+            if (text.isEmpty()) {
+                IdentificationLangDialog.showDialogWithError(getSupportFragmentManager(), getString(R.string.type_text_error));
+                return;
+            }
 
             LanguageIdentificator langIdentificator = new LanguageIdentificator(this);
             langIdentificator.identifyLanguage(text, new LanguageIdentificator.LanguageIdentificationCallback() {
